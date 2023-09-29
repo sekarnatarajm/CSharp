3ef7208d4fdd18cb11a90d10d49d693283428a33
sekarnatarajm <146414821+sekarnatarajm@users.noreply.github.com>
2023-09-29 12:05:39 +0530

    Second Commit

---
+++ CSharp-Basics/CSharp-Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
